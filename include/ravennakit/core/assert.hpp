df578433a2a5669b4fa5f8e67cc6b4ef534fa60c
Ruurd Adema <ruurd@owllab.nl>
2024-09-27 22:56:24 +0200

    Add RAV_ASSERT switched

--- include/ravennakit/core/assert.hpp
+++ include/ravennakit/core/assert.hpp
@@ -12,5 +12,48 @@
 
 #include <cassert>
 
-#define RAV_ASSERT(condition, message) assert(condition && message)
-#define RAV_ASSERT_FALSE(message) assert(message && false)
+#include "exception.hpp"
+#include "log.hpp"
+
+// #define RAV_LOG_ON_ASSERT 1
+// #define RAV_THROW_EXCEPTION_ON_ASSERT 1
+// #define RAV_ABORT_ON_ASSERT 1
+
+#ifndef RAV_LOG_ON_ASSERT
+    #define RAV_LOG_ON_ASSERT 0
+#endif
+
+#ifndef RAV_THROW_EXCEPTION_ON_ASSERT
+    #define RAV_THROW_EXCEPTION_ON_ASSERT 0
+#endif
+
+#ifndef RAV_ABORT_ON_ASSERT
+    #define RAV_ABORT_ON_ASSERT 0
+#endif
+
+#define LOG_IF_ENABLED(msg)  \
+    if (RAV_LOG_ON_ASSERT) { \
+        RAV_CRITICAL(msg);   \
+    }
+
+#define THROW_EXCEPTION_IF_ENABLED(msg)  \
+    if (RAV_THROW_EXCEPTION_ON_ASSERT) { \
+        RAV_THROW_EXCEPTION(msg);        \
+    }
+
+#define ABORT_IF_ENABLED(msg)  \
+    if (RAV_ABORT_ON_ASSERT) { \
+        abort();               \
+    }
+
+#define RAV_ASSERT(condition, message)                                \
+    do {                                                              \
+        assert((condition) && (message));                             \
+        if (!(condition)) {                                           \
+            LOG_IF_ENABLED("Assertion failure: " message)             \
+            THROW_EXCEPTION_IF_ENABLED("Assertion failure: " message) \
+            ABORT_IF_ENABLED(message)                                 \
+        }                                                             \
+    } while (false)
+
+#define RAV_ASSERT_FALSE(message) RAV_ASSERT(false, message)
