bd6366c477839657e84907321d3260f559d7ca2c
Ruurd Adema <ruurd@owllab.nl>
2025-03-14 23:28:26 +0100

    Add version info to footer (RAV-82)

--- docs/generate.py
+++ docs/generate.py
@@ -2,11 +2,35 @@
 import subprocess
 from pathlib import Path
 
+import pygit2
+
 script_path = Path(__file__)
 script_dir = script_path.parent
 
+# Git version
+repo = pygit2.Repository(path='.')
+git_version = repo.describe(pattern='v*')
+git_branch = repo.head.shorthand
+
+
+def replace_in_file(source_file, destination_file, old_string, new_string):
+    with open(source_file, "r", encoding="utf-8") as file:
+        content = file.read()
+
+    # Replace occurrences of the old string with the new string
+    content = content.replace(old_string, new_string)
+
+    # Write the modified content to the destination file
+    with open(destination_file, "w", encoding="utf-8") as file:
+        file.write(content)
+
+    print(f"Replaced '{old_string}' with '{new_string}' and saved to {destination_file}")
+
 
 def doxygen_docs():
+    replace_in_file(script_dir / 'assets' / 'footer.html.template',
+                    script_dir / 'assets' / 'footer.html',
+                    '%CUSTOM_FOOTER_TEXT%', f'RAVENNAKIT version {git_version} on branch {git_branch}')
     # Generate html docs
     subprocess.run(['doxygen', 'Doxyfile'], cwd=script_dir, check=True)
 
