cd268582e11e401900e93eb002a0b9def1cda1a5
Ruurd Adema <ruurd@owllab.nl>
2025-04-02 12:45:09 +0200

    Extend double buffer tests a bit (RAV-84)

--- test/ravennakit/core/sync/double_buffer.test.cpp
+++ test/ravennakit/core/sync/double_buffer.test.cpp
@@ -16,26 +16,87 @@
 TEST_CASE("DoubleBuffer") {
     static constexpr int k_num_iterations = 500000;
 
-    rav::DoubleBuffer<int> buffer;
+    SECTION("Basic operation") {
+        rav::DoubleBuffer<int> buffer;
+        buffer.update(42);
+        REQUIRE(buffer.get() == 42);
+        REQUIRE_FALSE(buffer.get().has_value());
+    }
 
-    std::thread producer([&] {
-        for (int i = 0; i < k_num_iterations; ++i) {
-            buffer.update(i);
-        }
-    });
+    SECTION("Equal speed") {
+        rav::DoubleBuffer<int> buffer;
 
-    std::thread consumer([&] {
-        int prev = -1;
-        for (int i = 0; i < k_num_iterations; ++i) {
-            if (auto value = buffer.get()) {
-                if (value <= prev) {
-                    throw std::runtime_error("Out of order value");
-                }
-                prev = *value;
+        std::thread producer([&] {
+            for (int i = 0; i < k_num_iterations; ++i) {
+                buffer.update(i);
             }
-        }
-    });
+        });
 
-    producer.join();
-    consumer.join();
+        std::thread consumer([&] {
+            int prev = -1;
+            for (int i = 0; i < k_num_iterations; ++i) {
+                if (auto value = buffer.get()) {
+                    if (value <= prev) {
+                        throw std::runtime_error("Out of order value");
+                    }
+                    prev = *value;
+                }
+            }
+        });
+
+        producer.join();
+        consumer.join();
+    }
+
+    SECTION("Faster producer") {
+        rav::DoubleBuffer<int> buffer;
+
+        std::thread producer([&] {
+            for (int i = 0; i < k_num_iterations; ++i) {
+                buffer.update(i);
+            }
+        });
+
+        std::thread consumer([&] {
+            int prev = -1;
+            for (int i = 0; i < k_num_iterations; ++i) {
+                if (auto value = buffer.get()) {
+                    if (value <= prev) {
+                        throw std::runtime_error("Out of order value");
+                    }
+                    prev = *value;
+                }
+                std::this_thread::sleep_for(std::chrono::microseconds(1));
+            }
+        });
+
+        producer.join();
+        consumer.join();
+    }
+
+    SECTION("Faster consumer") {
+        rav::DoubleBuffer<int> buffer;
+
+        std::thread producer([&] {
+            for (int i = 0; i < k_num_iterations; ++i) {
+                buffer.update(i);
+                std::this_thread::sleep_for(std::chrono::microseconds(1));
+            }
+        });
+
+        std::thread consumer([&] {
+            int prev = -1;
+            for (int i = 0; i < k_num_iterations; ++i) {
+                if (auto value = buffer.get()) {
+                    if (value <= prev) {
+                        throw std::runtime_error("Out of order value");
+                    }
+                    prev = *value;
+                }
+            }
+        });
+
+        producer.join();
+        consumer.join();
+    }
 }
