9bbbb938c6d75e49519a4cad48482486d6702bc1
Ruurd Adema <ruurd@owllab.nl>
2025-06-25 22:45:07 +0200

    Speed up HttpServer test (RAV-117)

--- test/ravennakit/core/net/http/http_server.test.cpp
+++ test/ravennakit/core/net/http/http_server.test.cpp
@@ -37,11 +37,12 @@ TEST_CASE("rav::HttpServer") {
             REQUIRE(response->result() == boost::beast::http::status::ok);
         });
 
-        client.get_async("/non-existent", [&server](auto response) {
+        client.get_async("/non-existent", [&io_context](auto response) {
             fmt::println("Response: {}", response.value());
             REQUIRE(response.has_value());
             REQUIRE(response->result() == boost::beast::http::status::not_found);
-            server.stop();
+            // server.stop(); TODO: We should do this, but there is a 5-second timeout somehow.
+            io_context.stop();
         });
 
         io_context.run();
@@ -93,11 +94,12 @@ TEST_CASE("rav::HttpServer") {
             REQUIRE(response->body() == "**");
         });
 
-        client.get_async("/non-existent", [&server](auto response) {
+        client.get_async("/non-existent", [&io_context](auto response) {
             REQUIRE(response.has_value());
             REQUIRE(response->result() == boost::beast::http::status::ok);
             REQUIRE(response->body() == "**");
-            server.stop();
+            // server.stop(); TODO: We should do this, but there is a 5-second timeout somehow.
+            io_context.stop();
         });
 
         io_context.run();
